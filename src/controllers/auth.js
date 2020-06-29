11ecc188610e30fa51f450b1f9f86f3dbacd7277
Heitor <heitormaveiro@yahoo.com.br>
2020-06-29 14:53:45 -0300

    storing account into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const {Account} = require('../models');
 
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/signin', (req, res)=> {
     return res.json('sign In')
 });
 
-router.get('/signup', (req, res)=> {
-    return res.json('sign up')
+router.get('/signup', async (req, res)=> {
+    const email ='heitormaveiro@yahoo.com.br';
+    const password = '123456';
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+
+    const result = await Account.create({email, password: hash});
+    return res.json(result);
 });
 
 module.exports = router;
